ee6b29101620c84544fba5ff760e0e0dd0c14a67
Brady Latsha <brady.latsha@gmail.com>
2017-05-26 01:55:26 -0400

    minor changes to config file

--- config/appconfig.py
+++ config/appconfig.py
@@ -8,17 +8,18 @@ log_dir = os.path.abspath(os.path.join(__file__, '..', '..', 'logs'))
 # endregion
 
 # region Plex configuration
-plex_tv_url = 'https://plex.tv'
 plex_config_file = os.path.abspath(os.path.join(__file__, '..', '..', '.plex'))
 plex_token = None
-plex_auth_url = auth_url = 'https://plex.tv/users/sign_in.json'
+plex_server_id = None
 plex_headers = {
     'X-Plex-Product': 'Plex Library Util',
     'X-Plex-Version': '1.0',
     'X-Plex-Client-Identifier': str(uuid.uuid4())
 }
-plex_server_id = None
-plex_shared_servers_url = 'https://plex.tv/api/servers/%s/shared_servers'
-plex_users_url = 'https://plex.tv/api/users'
-plex_libraries_url = 'https://plex.tv/api/servers/%s'
+# Plex urls
+plex_tv_url = 'https://plex.tv'
+plex_auth_url = auth_url = plex_tv_url + '/users/sign_in.json'
+plex_shared_servers_url = plex_tv_url + '/api/servers/%s/shared_servers'
+plex_users_url = plex_tv_url + '/api/users'
+plex_libraries_url = plex_tv_url + '/api/servers/%s'
 # endregion
